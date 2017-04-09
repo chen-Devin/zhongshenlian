c4506daae010ca2f2a3b11ca082791461ac6f6b0
Liulu <tlgliulu@163.com>
2017-04-09 20:40:46 +0800

    招投标信息面板初始化

--- src/main/router/marketDepartment/bidInforList.vue
+++ src/main/router/marketDepartment/bidInforList.vue
@@ -1,10 +1,36 @@
 <template>
-
+	<div class="main">
+	  <crumbs v-bind:paths="paths"></crumbs>
+	  <form class="form-inline">
+	  <div class="form-group">
+	    <label class="sr-only" for="exampleInputEmail3">Email address</label>
+	    <input type="email" class="form-control" id="exampleInputEmail3" placeholder="请输入关键字">
+	  </div>
+	  <button type="submit" class="btn btn-default">搜索</button>
+	</form>
+	</div>
+	
 </template>
 
 <script>
+
+import axios from 'axios';
+import qs from 'qs';
+
+import crumbs from '../../component/crumbs.vue';
+
 export default {
     name: 'bidInforListMarket',
+    data() {
+    	return {
+    		paths: [
+        		{name: '招投标信息看板', url: '/customer-infor-list-leader', present: true}
+      		]
+    	};
+    },
+    components: {
+    	crumbs
+    }
 }
 </script>
 
