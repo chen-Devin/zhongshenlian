f9a30c16976182754e91168495c26fcd672c1fc7
Abby <352920370@qq.com>
2017-04-19 19:30:05 +0800

    123

---
+++ src/main/router/commonUse/ruleRegulationAdd.vue
@@ -0,0 +1,116 @@
+<template>
+<div class="main">
+    <crumbs v-bind:paths="paths"></crumbs>
+    <card class="detailCard">
+      <div class="getDetail" v-bind="ruleAdd">
+          <input class="inputTitle" type="text" v-model="ruleAdd.title"/>
+          <textarea name="textarea" onpropertychange="this.style.height = this.scrollHeight + 'px';"
+          oninput="this.style.height = this.scrollHeight + 'px';" class="inputContent"  v-model="ruleAdd.content"></textarea>
+          <div class="isSaveBtn text-right">
+            <button type="button" class="btn btn-primary" @click="saveEdit()">保存</button>
+            <button type="button" class="btn btn-defalt" @click="canceled()">取消</button>
+          </div>
+      </div>
+   </card>
+</div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import qs from 'qs';
+    import router from '../index.js';
+
+    import crumbs from '../../component/crumbs.vue';
+    import card from '../../component/card.vue';
+    export default {
+        name: 'ruleRegulationAdd',
+        data() {
+            return {
+              paths: [
+                { name: '规章制度', url: '/rule-regulation', present: false },
+                { name: '录入制度', url: `/rule-regulation-add`, present: true }
+              ],
+              ruleAdd: {
+                  id: "",
+                  title: "",
+                  content: ""
+              }
+            }
+        },
+        methods: {
+            saveEdit() {
+                axios({
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                method: 'post',
+                url: '/service',
+                data: qs.stringify({
+                  data: (() => {
+                    var obj = {
+                      command: 'addOrEditRegulations',
+                      platform: 'web',
+                      regulationsId: this.ruleAdd.id,
+                      title: this.ruleAdd.title,
+                      content: this.ruleAdd.content
+                    };
+                    return JSON.stringify(obj);
+                  })()
+                })
+              }).then((rep)=>{
+                  if (rep.data.statusCode === '10001') {
+                    //this.ruleAdd.id = rep.data.id
+                    alert('保存成功');
+                    //加一个弹出框，然后加一个跳转
+                  }
+              }, (rep)=>{});
+            },
+            saveEdit(){
+              //alert(this.ruleAdd.title)
+            },
+            canceled() {
+                this.$router.push('/rule-regulation');
+            }
+        },
+        components: {
+            crumbs,
+            card
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+    .main {
+        .getDetail {
+            margin-top: 10px;
+            .inputContent {
+                width: 95%;
+                overflow-y: hidden;
+                min-height: 200px;
+                resize: none;
+            }
+            .inputTitle {
+                width: 50%;
+                padding: 10px;
+            }
+            .inputTitle,
+            .inputContent {
+                font-size: 16px;
+                display: block;
+                margin: 20px;
+                border: 1px solid #ccc;
+                outline: none
+            }
+            .isSaveBtn {
+                margin-top: 50px;
+                margin-left: 20px;
+                margin-bottom: 20px;
+                //width: 70%;
+                button {
+                    margin-right: 20px;
+                    width: 75px;
+                    height: 40px;
+                    font-size: 16px;
+                }
+            }
+        }
+    }
+</style>
--- src/main/router/commonUse/ruleRegulationDetail.vue
+++ src/main/router/commonUse/ruleRegulationDetail.vue
@@ -1,104 +1,126 @@
 <template>
 <div class="main">
     <crumbs v-bind:paths="paths"></crumbs>
-    <button class="btn btn-primary pull-right" type="button" @click="editRule">编辑</button>
-    <card id="ruleDetailSection">
-      <div class="ruleDetail" v-bind="detail">
-        <h3 class="ruleTitle">{{detail.title}}</h3>
-        <p>{{detail.content}}</p>
-        <p class="text-right releaseDepartment text-muted">{{detail.releaseDepartment}}</p>
-        <p class="text-right releaseTime text-muted">{{detail.releaseTime}}</p>
+    <button class="btn btn-primary pull-right" type="button" @click="editRule()">编辑</button>
+    <card>
+      <div class="getDetail" v-bind="detail">
+          <input class="inputTitle" type="text" v-model="detail.title" readonly/>
+          <textarea name="textarea" onpropertychange="this.style.height = this.scrollHeight + 'px';"
+          oninput="this.style.height = this.scrollHeight + 'px';" v-model="detail.content" class="inputContent" readonly></textarea>
+          <div class="releasePart text-right">
+             <p>{{detail.releaseDepartment}}</p>
+            <p>{{detail.releaseTime}}</p>
+          </div>
       </div>
    </card>
 </div>
 </template>
 
 <script>
-import axios from 'axios';
-import qs from 'qs';
-import router from '../index.js';
+    import axios from 'axios';
+    import qs from 'qs';
+    import router from '../index.js';
 
-import crumbs from '../../component/crumbs.vue';
-import card from '../../component/card.vue';
-export default {
-    name: 'ruleRegulationDetail',
-    data() {
-    	return {
-        paths: [
-          { name: '制度列表', url: '/rule-regulation', present: false },
-          { name: '制度详情', url: `/rule-regulation-detail/${this.$route.params.id}`, present: true }
-        ],
-        detail: {
-           id: this.$route.params.id,
-           title: "",
-           content: "",
-           releaseDepartment: "",
-           releaseTime: ""
-           //numbering: ""
-        }
-      }
-    },
-    created() {
-      this.getRuleDetail();
-    },
-    methods: {
-      getRuleDetail(){
-         axios({
-        method: 'get',
-        url: '/service',
-        params: {
-            data: (()=>{
-            var obj = {
-                command: 'getRegulationsInfo',
-                platform: 'web',
-                regulationsId: this.$route.params.id
+    import crumbs from '../../component/crumbs.vue';
+    import card from '../../component/card.vue';
+    export default {
+        name: 'ruleRegulationDetail',
+        data() {
+            return {
+               paths: [
+                { name: '规章制度', url: '/rule-regulation', present: false },
+                { name: '制度详情', url: `/rule-regulation-detail/${this.$route.params.id}`, present: true }
+              ],
+              detail: {
+                  id: this.$route.params.id,
+                  title: "",
+                  content: "",
+                  releaseDepartment: "",
+                  releaseTime: ""
+                      //numbering: ""
+              }
             }
-            return JSON.stringify(obj);
-            })()
         },
-    }).then((rep) => {
-            if (rep.data.statusCode === '10001') {
-              this.detail.title =  rep.data.data.title,
-              this.detail.content =  rep.data.data.content,
-              this.detail.releaseDepartment = rep.data.data.releaseDepartment,
-              this.detail.releaseTime = rep.data.data.releaseTime
-             // this.detail.numbering = rep.data.data.numbering
+        created() {
+            this.getRuleDetail();
+        },
+        methods: {
+            getRuleDetail() {
+                axios({
+                    method: 'get',
+                    url: '/service',
+                    params: {
+                        data: (() => {
+                            var obj = {
+                                command: 'getRegulationsInfo',
+                                platform: 'web',
+                                regulationsId: this.$route.params.id
+                            }
+                            return JSON.stringify(obj);
+                        })()
+                    },
+                }).then((rep) => {
+                    if (rep.data.statusCode === '10001') {
+                        this.detail.title = rep.data.data.title,
+                            this.detail.content = rep.data.data.content,
+                            this.detail.releaseDepartment = rep.data.data.releaseDepartment,
+                            this.detail.releaseTime = rep.data.data.releaseTime
+                            // this.detail.numbering = rep.data.data.numbering
+                    }
+                }, (rep) => {})
+            },
+            editRule() {
+                // alert(this.detail.id)
+                this.$router.push("/rule-regulation-edit/"+this.detail.id);
             }
-          }, (rep) =>{})
-      },
-      editRule(){
-        this.$router.push("/rule-regulation-edit");
-      }
-    },
-    components: {
-    	crumbs,
-    	card
+        },
+        components: {
+            crumbs,
+            card
+        }
     }
-}
 </script>
 
 <style lang="sass" scoped>
-.main{
-   #ruleDetailSection{
-    div{
-    .ruleTitle{
-        margin-bottom: 30px;
-      }
-    .releaseDepartment{
-        font-size: 16px;
-        margin-top: 40px;
+    .main {
+        button {
+            width: 75px;
+            height: 40px;
+            font-size: 16px;
+            margin-top: -50px;
+            margin-right: 20px;
+        }
+        .getDetail {
+            margin-top: 10px;
+            .inputContent {
+                width: 95%;
+                overflow-y: hidden;
+                min-height: 50px;
+                resize: none;
+                font-size: 16px;
+            }
+            .inputTitle {
+                width: 50%;
+
+                font-size: 18px;
+                font-weight: bold;
+            }
+            .inputTitle,
+            .inputContent {
+                display: block;
+                padding: 10px;
+                margin: 20px;
+                border: 1px solid #fff;
+                outline: none;
+            }
+            .releasePart {
+                //width: 70%;
+                font-size: 16px;
+                margin: 20px;
+                p {
+                    margin-top: 10px;
+                }
+            }
+        }
     }
-    .releaseTime{
-        font-size: 16px;
-      }
-    }
-  }
-    button{
-    width: 100px;
-    height: 40px;
-    font-size: 16px;
-    margin-top: -54px;
-    margin-right: 30px;
-  }
-}
 </style>
