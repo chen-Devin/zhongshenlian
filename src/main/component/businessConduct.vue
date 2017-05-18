0c52a64c206330351a835335f1e56043282f6a02
肖大宝 <xiaojin1992@126.com>
2017-05-18 19:52:40 +0800

    提交

--- src/main/component/businessConduct.vue
+++ src/main/component/businessConduct.vue
@@ -74,101 +74,6 @@ export default {
                 platform: 'web',
                 data: {
                   id: this.business.id,
-                  projectName: this.business.name,
-                  contractNo: this.business.number,
-                  requester: this.business.institution.customerName,
-                  requesterId: this.business.institution.id,
-                  requesterName: this.business.institution.name,
-                  requesterPhone: this.business.institution.telephone,
-                  reportPurpose: this.business.report.usage,
-                  startTime: this.business.time.start,
-                  endTime: this.business.time.end,
-                  totalAssets: this.business.institution.assetSize,
-                  contractAmount: this.business.contractAmount,
-                  contractPrice: this.business.contractPrice,
-                  reportCopies: this.business.report.amount,
-                  reportType: (() => {
-                    let out = [];
-                    for (let i = 0; i < this.business.report.type.length; i++) {
-                      let flag = false;
-                      let typeArray = [];
-                      for (let j = 0; j < this.business.report.type[i].words.length; j++) {
-                        if (this.business.report.type[i].words[j].state) {
-                          flag = true;
-                          typeArray.push({
-                            name: this.business.report.type[i].words[j].name
-                          });
-                        }
-                      }
-                      if (flag) {
-                        out.push({
-                          department: this.business.report.type[i].name,
-                          typeArray
-                        });
-                      }
-                    }
-                    return out;
-                  })(),
-                  businessType: this.business.type,
-                  projectStatus: this.business.projectStatus,
-                  projectManagerId: this.business.manager.id,
-                  projectManagerName: this.business.manager.name,
-                  //手动输入
-                  projectManager: this.business.manager.name,
-                  checkStartTime: this.business.auditTime.start,
-                  checkEndTime: this.business.auditTime.end,
-                  cooperationDepartment: (() => {
-                    let out = {};
-                    if (this.business.departmentCoop.name === '有部门合作') {
-                      out.mainDepartment = this.business.departmentCoop.departments.main.name;
-                      out.mainRate = this.business.departmentCoop.departments.main.percentage;
-                      out.otherArray = [];
-                      for (let i = 0; i < this.business.departmentCoop.departments.coop.length; i++) {
-                        out.otherArray.push({
-                          cooperation: this.business.departmentCoop.departments.coop[i].name,
-                          cooperationRate: this.business.departmentCoop.departments.coop[i].percentage
-                        });
-                      }
-                    }
-                    return out;
-                  })(),
-                  trialTeacherId: this.business.reviewCPA.id,
-                  trialTeacherName: this.business.reviewCPA.name,
-                  //手动输入
-                  trialTeacher: this.business.reviewCPA.name,
-                  trialAssistantId: this.business.reviewAssistant.id,
-                  trialAssistantName: this.business.reviewAssistant.name,
-                  //手动输入
-                  trialAssistant: this.business.reviewAssistant.name,
-                  lastOffice: this.business.lastOffice,
-                  getWay: this.business.getWay,
-                  contractType: (() => {
-                    let out = {
-                      type: this.business.contractType.name,
-                      mainBasicName: this.business.contractType.basicFee.main.name,
-                      mainBasicRate: this.business.contractType.basicFee.main.percentage,
-                      mainEfficiencyName: this.business.contractType.benefitFee.main.name,
-                      mainEfficiencyRate: this.business.contractType.benefitFee.main.percentage,
-                      subBasicArray: [],
-                      subEfficiencyArray: []
-                    };
-                    for (let i = 0; i < this.business.contractType.basicFee.depend.length; i++) {
-                      out.subBasicArray.push({
-                        name: this.business.contractType.basicFee.depend[i].name,
-                        rate: this.business.contractType.basicFee.depend[i].percentage
-                      });
-                    }
-                    for (let i = 0; i < this.business.contractType.benefitFee.depend.length; i++) {
-                      out.subEfficiencyArray.push({
-                        name: this.business.contractType.benefitFee.depend[i].name,
-                        rate: this.business.contractType.benefitFee.depend[i].percentage
-                      });
-                    }
-                    return out;
-                  })(),
-                  annexArray: this.business.files,
-                  contractAnnexArray: this.business.contracts,
-                  projectApproverArray: this.business.projectApproverArray,
                   projectSchduleArray: (() => {
                     let arr = [];
                     for (let i = 0; i < this.business.schdules.length; i++) {
@@ -179,9 +84,7 @@ export default {
                       arr.push(obj);
                     }
                     return arr;
-                  })(),
-                  projectBillingArray: this.business.bills,
-                  projectOperatingArray: this.business.projectOperatingArray
+                  })()
                 }
               };
               return JSON.stringify(obj);
