33135493e247463927b7ea2e7c02aab136131e82
Liu Lu <tlgliulu@163.com>
2017-07-31 18:03:11 +0800

    隐藏风评意见

--- src/main/router/salesDepartment/businessReviewDetail.vue
+++ src/main/router/salesDepartment/businessReviewDetail.vue
@@ -713,7 +713,9 @@ export default {
       this.leaderAdivces = [];
       for (let i = 0; i < this.business.projectApproverArray.length; i++) {
         if (this.business.projectApproverArray[i].department === '风险评估部') {
+          this.business.projectApproverArray[i].approverName = '';
           this.riskAdvices.push(this.business.projectApproverArray[i]);
+          console.log(this.riskAdvices)
         } else {
           this.leaderAdivces.push(this.business.projectApproverArray[i]);
         }
